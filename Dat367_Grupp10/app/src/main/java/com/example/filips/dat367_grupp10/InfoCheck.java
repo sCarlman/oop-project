691d74de898891846ca45534d46edc3edf2c5a70
Isaac Gonzalez <isaac.gonzalez1995@gmail.com>
2015-04-15 10:04:35 +0200

    Hóla
    Profile and InfoCheck classes

---
+++ Dat367_Grupp10/app/src/main/java/com/example/filips/dat367_grupp10/InfoCheck.java
@@ -0,0 +1,40 @@
+package com.example.filips.dat367_grupp10;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * Created by Isaac on 2015-04-01.
+ * help class for checking valid inputs
+ */
+public class InfoCheck {
+
+    public static boolean isAlphabetic(String name) {
+        char[] chars = name.toCharArray();
+        for (char c : chars) {
+            if(!Character.isLetter(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isEmailCorrect(String email) {
+        Pattern p = Pattern.compile(".+@.+\\.[a-z]+");
+        Matcher m = p.matcher(email);
+        boolean emailValid = m.matches();
+        return emailValid;
+    }
+
+    public static boolean isNumeric(String phone) {
+        char[] chars = phone.toCharArray();
+        //method requires symbols like "-" to be consumed
+        for (char c : chars) {
+            if(!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
