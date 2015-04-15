c5d12fbbc0370aa8d0a6c485d8afd0688aa98f0f
Seholm <filipslottner@gmail.com>
2015-04-15 11:55:59 +0200

    Mapview!!

--- Dat367_Grupp10/app/src/main/java/com/example/filips/dat367_grupp10/MainActivity.java
+++ Dat367_Grupp10/app/src/main/java/com/example/filips/dat367_grupp10/MainActivity.java
@@ -1,16 +1,48 @@
 package com.example.filips.dat367_grupp10;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-
-public class MainActivity extends Activity {
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void openMapView(View view){
+
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
 }
--- Dat367_Grupp10/app/src/main/java/com/example/filips/dat367_grupp10/MapsActivity.java
+++ Dat367_Grupp10/app/src/main/java/com/example/filips/dat367_grupp10/MapsActivity.java
@@ -17,7 +17,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_maps);
 
 
     }
