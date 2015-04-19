2970055df7183683dec448d2d74ab3e96b1587f6
Filip Larsson <filip.larsson.vbg@gmail.com>
2015-04-19 15:27:59 +0200

    Ad is created when clicking create ad btn

--- Dat367_Grupp10/app/src/main/java/com/example/filips/dat367_grupp10/CreateAdActivity.java
+++ Dat367_Grupp10/app/src/main/java/com/example/filips/dat367_grupp10/CreateAdActivity.java
@@ -4,9 +4,13 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 
-public class CreateAdActivity extends ActionBarActivity {
+public class CreateAdActivity extends ActionBarActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +39,37 @@ public class CreateAdActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View v){
+
+    }
+
+    public void createAd(View v){
+       
+        EditText firstName = (EditText)findViewById(R.id.firstNameEditText);
+        EditText lastName = (EditText)findViewById(R.id.lastNameEditText);
+        EditText email = (EditText)findViewById(R.id.emailEditText);
+        EditText phone = (EditText)findViewById(R.id.phoneEditText);
+        EditText address = (EditText)findViewById(R.id.adressEditText);
+        EditText description = (EditText)findViewById(R.id.discriptionEditText);
+
+        Profile newProfile = new Profile(firstName.getText().toString(), lastName.getText().toString(),
+                email.getText().toString(),phone.getText().toString(),address.getText().toString());
+
+        RadioButton gardenRadioButton = (RadioButton) findViewById(R.id.gardenRadioButton);
+        RadioButton labourRadioButton = (RadioButton) findViewById(R.id.labourRadioButton);
+        RadioButton otherRadioButton = (RadioButton) findViewById(R.id.otherRadioButton);
+
+        if (gardenRadioButton.isSelected()){
+            Advertisement newAd = new Advertisement(newProfile, description.getText().toString(), Category.GARDEN);
+        }else if(labourRadioButton.isSelected()){
+            Advertisement newAd = new Advertisement(newProfile, description.getText().toString(), Category.LABOUR);
+        }else{
+            Advertisement newAd = new Advertisement(newProfile, description.getText().toString(), Category.OTHER);
+        }
+
+        Toast.makeText(this, "Din annons har publicerats", Toast.LENGTH_LONG).show();
+
     }
 }
