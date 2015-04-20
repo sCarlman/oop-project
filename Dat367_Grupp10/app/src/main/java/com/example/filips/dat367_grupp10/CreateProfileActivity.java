484b6d7214c7c7f889a2cdb4aa1ec6c4b1818f18
Isaac Gonzalez <isaac.gonzalez1995@gmail.com>
2015-04-20 09:06:47 +0200

    halfway through creating profile
    time to die

--- Dat367_Grupp10/app/src/main/java/com/example/filips/dat367_grupp10/CreateProfileActivity.java
+++ Dat367_Grupp10/app/src/main/java/com/example/filips/dat367_grupp10/CreateProfileActivity.java
@@ -4,16 +4,43 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 
-public class CreateProfileActivity extends ActionBarActivity {
+public class CreateProfileActivity extends ActionBarActivity implements View.OnClickListener{
+    private EditText firstNameEditText;
+    private EditText lastNameEditText;
+    private EditText emailEditText;
+    private EditText phoneEditText;
+    private EditText passwordEditText;
+    private Button createProfileButton;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String preferredLocation;
+    private String password;
+    private Profile newProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createprofile);
+        firstNameEditText = (EditText) findViewById(R.id.firstNameEditText);
+        lastNameEditText = (EditText) findViewById(R.id.lastNameEditText);
+        emailEditText = (EditText) findViewById(R.id.emailEditText);
+        phoneEditText = (EditText) findViewById(R.id.phoneEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        createProfileButton = (Button) findViewById(R.id.createProfileButton);
+        createProfileButton.setOnClickListener(this);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +62,17 @@ public class CreateProfileActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        firstName = firstNameEditText.getText().toString();
+        lastName = lastNameEditText.getText().toString();
+        email = emailEditText.getText().toString();
+        phone = phoneEditText.getText().toString();
+        password = passwordEditText.getText().toString();
+        preferredLocation = ""; //tills vidare tom...
+        newProfile = new Profile(firstName, lastName, email, phone, preferredLocation, password);
+
     }
 }
--- Dat367_Grupp10/app/src/main/java/com/example/filips/dat367_grupp10/Profile.java
+++ Dat367_Grupp10/app/src/main/java/com/example/filips/dat367_grupp10/Profile.java
@@ -1,5 +1,7 @@
 package com.example.filips.dat367_grupp10;
 
+import java.util.List;
+
 /**
  * Created by Isaac on 2015-04-01.
  * A profile is needed to post ads
@@ -11,9 +13,11 @@ public class Profile {
     private String email;
     private String phone;
     private String preferredLocation;
+    private String password;
+    private static List<Profile> ProfileList;
 
     public Profile(String firstName, String lastName, String email, String phone,
-                   String preferredLocation) {
+                   String preferredLocation, String password) {
         if (InfoCheck.isAlphabetic(firstName)) {
             this.firstName = firstName;
         }else {
@@ -34,8 +38,10 @@ public class Profile {
         }else {
             //TODO: throw some exception.
         }
-        //TODO check preferredLocation??
+        //TODO check preferredLocation
         this.preferredLocation = preferredLocation;
+        //TODO check password
+        this.password = password;
     }
 
     public String getFirstName() {
@@ -58,6 +64,8 @@ public class Profile {
         return preferredLocation;
     }
 
+    public String getPassword() { return password; }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
@@ -77,4 +85,6 @@ public class Profile {
     public void setPreferredLocation(String preferredLocation) {
         this.preferredLocation = preferredLocation;
     }
+
+    public void setPassword(String password) { this.password = password; }
 }
