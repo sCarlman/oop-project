2c57bc0e2391cd5442296b997fd017cd1ba285ad
sCarlman <carl.albertsson@gmail.com>
2015-05-20 14:19:50 +0200

    JUnit test

---
+++ Dat367_Grupp10/app/src/test/java/edu/ctl/pinjobs/profile/ProfileTest.java
@@ -0,0 +1,16 @@
+package edu.ctl.pinjobs.profile;
+
+import org.junit.Test;
+
+/**
+ * Created by Albertsson on 15-05-20.
+ */
+public class ProfileTest {
+
+    @Test
+    public void testCreateProfile(){
+        assert true;
+    }
+
+
+}
