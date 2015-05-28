7e842909cf24fcc4318d9f8b921af32c884ed704
Isaac Gonzalez <isaac.gonzalez1995@gmail.com>
2015-05-28 23:51:05 +0200

    updated refresh adlist now implemented
    funkar, men sääämst ju! :walking:  :no_good:  :no_smoking:

--- Dat367_Grupp10/app/src/main/java/edu/ctl/pinjobs/Handler/view/ListView.java
+++ Dat367_Grupp10/app/src/main/java/edu/ctl/pinjobs/Handler/view/ListView.java
@@ -25,20 +25,23 @@ public class ListView{
     private android.widget.ListView listView;
     private Context context;
     private AdapterView.OnItemClickListener clickListener;
+    private SwipeRefreshLayout.OnRefreshListener onRefreshListener;
     private HandlerLocationUtils locationUtils;
     private SwipeRefreshLayout swipeRefreshList;
 
-    public ListView(Activity activity,AdapterView.OnItemClickListener clickListener){
+    public ListView(Activity activity,AdapterView.OnItemClickListener clickListener,
+                    SwipeRefreshLayout.OnRefreshListener onRefreshListener){
 
         this.listView = (android.widget.ListView) activity.findViewById(R.id.adListView);
         this.swipeRefreshList = (SwipeRefreshLayout) activity.findViewById(R.id.swipeRefreshList);
         this.context = activity.getApplicationContext();
         this.clickListener = clickListener;
+        this.onRefreshListener = onRefreshListener;
         this.locationUtils = new HandlerLocationUtils();
 
     }
 
-    public void setupView(final List<IAdvertisement> adList, final int id){
+    public void setupView(final List<IAdvertisement> adList, int id){
         List<String> titleList = new ArrayList<String>();
         final LatLng currentPosition = LocationUtils.getCurrentLocation(context);
         for(IAdvertisement ad : adList){
@@ -49,19 +52,8 @@ public class ListView{
         listView.setAdapter(adapter);
         listView.invalidate();
         listView.setOnItemClickListener(clickListener);
-        //calls refreshList when swiped to refresh.
-        swipeRefreshList.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                refreshList(context,id);
-            }
-        });
+        swipeRefreshList.setOnRefreshListener(onRefreshListener);
 
-    }
-
-    private void refreshList(Context context, int id) {
-        //TODO: Fetch adlist from parse, pls!
-        //Here's how to do: https://www.bignerdranch.com/blog/implementing-swipe-to-refresh/
     }
 
     private String setMessage(IAdvertisement ad,LatLng currentPos){
@@ -69,7 +61,7 @@ public class ListView{
         String distance = "" + locationUtils.calculateDistanceFromPosition(currentPos.latitude,ad.getLatitude(),currentPos.longitude,ad.getLongitude());
         int index = distance.indexOf('.');
         distance = distance.substring(0, index + 2);
-        return message + "      " + distance + " km bort";
+        return message + "\n" + distance + "km bort";
     }
 
 }
